c2115ddc6eeb8c90d42eed37762f3a1af11b501d
evio <evio@vip.qq.com>
2018-09-28 11:06:24 +0800

    update

--- app.js
+++ app.js
@@ -1,53 +1,62 @@
-const fs = require('fs');
-const path = require('path');
+const http = require('http');
 const Socket = require('socket.io');
+const { EventEmitter } = require('events');
+
 module.exports = async app => {
-  app.loader.addCompiler('socket', dirs => {
-    const result = {};
-    dirs.forEach(dir => {
-      const dic = path.resolve(dir.pathname, 'app/io');
-      readDir(dic, dic, result);
-    });
-    app.__sockets__ = result;
+  Object.defineProperty(app.context, 'routerPath', {
+    get() {
+      return this.req.socketPath;
+    }
+  });
+  Object.defineProperty(app.context, 'socket', {
+    get() {
+      return this.req.socket || new EventEmitter();
+    }
   });
   app.starting(async server => {
     app.io = Socket(server);
     app.context.io = app.io;
-    if (app.__sockets__) {
-      for (const res in app.__sockets__) {
-        let result = app.__sockets__[res];
-        if (typeof result === 'function') {
-          result = await result(app);
-        }
-        if (result && typeof result === 'object') {
-          const room = app.io.of(res);
-          for (const i in result) {
-            if (typeof result[i] === 'function') {
-              room.on(i, result[i]);
+    const callback = app.callback();
+    
+    for (let i = 0; i < app.router.sockets.length; i++) {
+      const roomName = app.router.sockets[i];
+      const room = app.io.of(roomName);
+      
+      room.on('connection', socket => {
+        const request = socket.request;
+        const response = new http.ServerResponse(request);
+        
+        request.socket = socket;
+        request.method = 'SOCKET';
+        request.socketPath = roomName;
+        
+        callback(request, response).then(response => {
+          const ctx = response.socketContext;
+          const target = response.socketResult;
+          
+          socket.on('commander', data => {
+            if (!data) return;
+            ctx.socketBody = data.body;
+            if (data.event && typeof target[data.event] === 'function') {
+              target[data.event].call(target, ctx).catch(e => app.console.error(e.message));
             }
+          });
+          
+          socket.on('disconnect', () => {
+            if (typeof target.disconnect === 'function') {
+              target.disconnect.call(target, ctx).catch(e => app.console.error(e.message));
+            }
+          });
+  
+          if (typeof target.connect === 'function') {
+            target.connect.call(target, ctx)
+              .then(() => socket.emit('commander_connect_success', { timestamp: Date.now() }))
+              .catch(e => app.console.error(e.message));
+          } else {
+            socket.emit('commander_connect_success', { timestamp: Date.now() });
           }
-        }
-      }
+        }).catch(e => socket.emit('commander_connect_error', { message: e.message }));
+      });
     }
   });
 };
-
-function readDir(base, dir, result) {
-  if (!fs.existsSync(dir)) return;
-  fs.readdirSync(dir).forEach(file => {
-    const realPath = path.resolve(dir, file);
-    const target = fs.statSync(realPath);
-    if (target.isFile()) {
-      if (/.js$/.test(file)) {
-        const relative = '/' + path.relative(base, realPath);
-        const moduleExports = require(realPath);
-        const dirname = path.dirname(relative);
-        const basename = path.basename(relative, '.js');
-        const name = dirname === '/' ? dirname + basename : dirname + '/' + basename;
-        result[name] = moduleExports;
-      }
-    } else {
-      readDir(base, realPath, result);
-    }
-  })
-}
